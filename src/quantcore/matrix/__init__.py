0abf742d09f62b8d35099f595791e649271267f1
Marc-Antoine Schmidt <marc-antoine.schmidt@quantco.com>
2020-07-10 16:56:28 -0400

    working prototype

--- src/quantcore/matrix/__init__.py
+++ src/quantcore/matrix/__init__.py
@@ -1,6 +1,7 @@
 from .categorical_matrix import CategoricalMatrix
 from .dense_matrix import DenseMatrix
 from .matrix_base import MatrixBase, one_over_var_inf_to_val
+from .pandas import from_pandas
 from .sparse_matrix import SparseMatrix
 from .split_matrix import SplitMatrix, csc_to_split
 from .standardized_mat import StandardizedMatrix
@@ -14,4 +15,5 @@ __all__ = [
     "CategoricalMatrix",
     "csc_to_split",
     "one_over_var_inf_to_val",
+    "from_pandas",
 ]
