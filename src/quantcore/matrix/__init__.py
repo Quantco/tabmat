2b0eb0f1404c2ddae53cb20ab2232bdb81e91703
Marc-Antoine Schmidt <marc-antoine.schmidt@quantco.com>
2020-07-20 13:59:29 -0400

    pandas to matrix function (#16)
    * working prototype
    
    * more efficient implementation + docstring
    
    * added simple test
    
    * keep ordering
    
    * fix test
    
    * let user choose categorical location
    
    * typo

--- src/quantcore/matrix/__init__.py
+++ src/quantcore/matrix/__init__.py
@@ -1,4 +1,5 @@
 from .categorical_matrix import CategoricalMatrix
+from .constructor import from_pandas
 from .dense_matrix import DenseMatrix
 from .matrix_base import MatrixBase, one_over_var_inf_to_val
 from .sparse_matrix import SparseMatrix
@@ -14,4 +15,5 @@ __all__ = [
     "CategoricalMatrix",
     "csc_to_split",
     "one_over_var_inf_to_val",
+    "from_pandas",
 ]
