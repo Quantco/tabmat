19dce9305f8671d365b6944e21d896a16a28ce8d
Elizabeth Santorella <elizabeth.santorella@gmail.com>
2020-07-29 14:21:23 -0400

    int -> integegral for long compatibility

--- src/quantcore/matrix/util.py
+++ src/quantcore/matrix/util.py
@@ -1,9 +1,20 @@
+from typing import Optional, Tuple
+
 import numpy as np
 
 
-def setup_restrictions(shape, rows, cols):
+def setup_restrictions(
+    shape: Tuple[int, int],
+    rows: Optional[np.ndarray],
+    cols: Optional[np.ndarray],
+    dtype=np.int32,
+) -> Tuple[np.ndarray, np.ndarray]:
     if rows is None:
-        rows = np.arange(shape[0], dtype=np.int32)
+        rows = np.arange(shape[0], dtype=dtype)
+    elif rows.dtype != dtype:
+        rows = rows.astype(dtype)
     if cols is None:
-        cols = np.arange(shape[1], dtype=np.int32)
+        cols = np.arange(shape[1], dtype=dtype)
+    elif cols.dtype != dtype:
+        cols = cols.astype(dtype)
     return rows, cols
