f57f26ce7d24e1812af903b600da0739996f8d86
Elizabeth Santorella <elizabeth.santorella@gmail.com>
2020-07-30 16:06:37 -0400

    64 bit for sparse x dense

--- src/quantcore/matrix/ext/sparse.pyx
+++ src/quantcore/matrix/ext/sparse.pyx
@@ -151,31 +151,38 @@ def csc_rmatvec(XT, floating[:] v, integral[:] rows, integral[:] cols):
     return out
 
 cdef extern from "sparse_helpers.cpp":
-    void _csr_denseC_sandwich[F](
-        F*, int*, int*, F*, F*, F*, int, int, int,
-        int*, int*, int*, int, int, int
+    void _csr_denseC_sandwich[I, F](
+        F*, I*, I*, F*, F*, F*, I, I, I,
+        I*, I*, I*, I, I, I
     ) nogil
-    void _csr_denseF_sandwich[F](
-        F*, int*, int*, F*, F*, F*, int, int, int,
-        int*, int*, int*, int, int, int
+    void _csr_denseF_sandwich[I, F](
+        F*, I*, I*, F*, F*, F*, I, I, I,
+        I*, I*, I*, I, I, I
     ) nogil
 
-def csr_dense_sandwich(A, np.ndarray B, floating[:] d, int[:] rows, int[:] A_cols, int[:] B_cols):
+def csr_dense_sandwich(
+        A,
+        np.ndarray B,
+        floating[:] d,
+        integral[:] rows,
+        integral[:] A_cols,
+        integral[:] B_cols
+    ):
     # computes where (A.T * d) @ B
     # assumes that A is in csr form
     cdef floating[:] Adata = A.data
-    cdef int[:] Aindices = A.indices
-    cdef int[:] Aindptr = A.indptr
+    cdef integral[:] Aindices = A.indices
+    cdef integral[:] Aindptr = A.indptr
 
     # A has shape (n, m)
     # B has shape (n, r)
-    cdef int m = A.shape[1]
-    cdef int n = d.shape[0]
-    cdef int r = B.shape[1]
+    cdef integral m = A.shape[1]
+    cdef integral n = d.shape[0]
+    cdef integral r = B.shape[1]
 
-    cdef int nr = rows.shape[0]
-    cdef int nAc = A_cols.shape[0]
-    cdef int nBc = B_cols.shape[0]
+    cdef integral nr = rows.shape[0]
+    cdef integral nAc = A_cols.shape[0]
+    cdef integral nBc = B_cols.shape[0]
 
     out = np.zeros((nAc, nBc), dtype=A.dtype)
     if nr == 0 or nAc == 0 or nBc == 0 or (Aindptr[A.indptr.shape[0] - 1] - Aindptr[0]) == 0:
@@ -186,9 +193,9 @@ def csr_dense_sandwich(A, np.ndarray B, floating[:] d, int[:] rows, int[:] A_col
 
     cdef floating* Bp = <floating*>B.data
 
-    cdef int* rowsp = &rows[0];
-    cdef int* A_colsp = &A_cols[0];
-    cdef int* B_colsp = &B_cols[0];
+    cdef integral* rowsp = &rows[0];
+    cdef integral* A_colsp = &A_cols[0];
+    cdef integral* B_colsp = &B_cols[0];
 
     if B.flags['C_CONTIGUOUS']:
         _csr_denseC_sandwich(
