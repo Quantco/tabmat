138cacfc02b4eeb3895d1f028bad3e78f69fff1c
Uwe L. Korn <uwe.korn@quantco.com>
2021-03-25 11:49:16 +0100

    Set SYSROOT to latest MacOS SDK

--- .github/workflows/macos-conda-build.sh
+++ .github/workflows/macos-conda-build.sh
@@ -2,6 +2,7 @@
 
 set -exo pipefail
 
+export CONDA_BUILD_SYSROOT=$(xcrun --sdk macosx --show-sdk-path)
 mamba install -y conda-build
 if grep -q "osx-arm64" .ci_support/${CONDA_BUILD_YML}.yaml; then
   CONDA_BUILD_ARGS="--no-test"
