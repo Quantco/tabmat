ce873616fbe99fc3f29a69bcd7c3b3e6bc94310d
Uwe L. Korn <uwe.korn@quantco.com>
2021-03-25 11:33:46 +0100

    Strip yaml suffix from CONDA_BUILD_YML

--- .github/workflows/macos-conda-build.sh
+++ .github/workflows/macos-conda-build.sh
@@ -3,7 +3,7 @@
 set -exo pipefail
 
 mamba install -y conda-build
-if grep -q "osx-arm64" .ci_support/${CONDA_BUILD_YML}; then
+if grep -q "osx-arm64" .ci_support/${CONDA_BUILD_YML}.yaml; then
   CONDA_BUILD_ARGS="--no-test"
 fi
-conda build -m .ci_support/${CONDA_BUILD_YML} conda.recipe ${CONDA_BUILD_ARGS:-}
+conda build -m .ci_support/${CONDA_BUILD_YML}.yaml conda.recipe ${CONDA_BUILD_ARGS:-}
