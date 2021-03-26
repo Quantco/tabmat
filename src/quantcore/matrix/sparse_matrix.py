4d1a0b0d8f22de9f56d47554515df1b9b29143d6
Ben Thompson <t.ben.thompson@gmail.com>
2021-03-26 16:40:40 -0400

    Use builtin scipy.sparse dot product when MKL isn't available.

--- src/quantcore/matrix/sparse_matrix.py
+++ src/quantcore/matrix/sparse_matrix.py
@@ -21,13 +21,17 @@ from .util import (
 
 if platform.machine() in {"arm64", "aarch64"}:
 
-    def dot_product_mkl(matrix_a, matrix_b, out):
-        return np.dot(matrix_a, matrix_b, out=out)
+    def dot_product_maybe_mkl(matrix_a, matrix_b, out):
+        mult = matrix_a.dot(matrix_b)
+        if out is None:
+            return mult
+        out[:] += mult
+        return out
 
 
 else:
     # Intel MKL obviously is only available on Intel-based architectures
-    from sparse_dot_mkl import dot_product_mkl  # type: ignore
+    from sparse_dot_mkl import dot_product_mkl as dot_product_maybe_mkl  # type: ignore
 
 
 class SparseMatrix(sps.csc_matrix, MatrixBase):
@@ -142,10 +146,10 @@ class SparseMatrix(sps.csc_matrix, MatrixBase):
         unrestricted_cols = cols is None or len(cols) == self.shape[1]
         if unrestricted_rows and unrestricted_cols:
             if vec.ndim == 1:
-                return dot_product_mkl(X, vec, out=out)
+                return dot_product_maybe_mkl(X, vec, out=out)
             elif vec.ndim == 2 and vec.shape[1] == 1:
                 out_arr = None if out is None else out[:, 0]
-                return dot_product_mkl(X, vec[:, 0], out=out_arr)[:, None]
+                return dot_product_maybe_mkl(X, vec[:, 0], out=out_arr)[:, None]
             res = matrix_matvec(self, vec)
             if out is None:
                 return res
