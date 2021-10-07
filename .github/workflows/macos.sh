9967e1b20e016912adee8f0baf4a097d3149ff4c
Uwe L. Korn <uwe.korn@quantco.com>
2021-10-07 16:57:42 +0200

    Ensure the correct macos deployment target is set

--- .github/workflows/macos.sh
+++ .github/workflows/macos.sh
@@ -5,9 +5,9 @@ set -exo pipefail
 source ~/.profile
 mamba install -y yq jq
 
-mamba install -y yq
 yq -Y ". + {dependencies: [.dependencies[], \"python=${PYTHON_VERSION}\"] }" environment.yml > /tmp/environment.yml
 mamba env create -f /tmp/environment.yml
 conda activate $(yq -r .name environment.yml)
+export MACOSX_DEPLOYMENT_TARGET=10.9
 pip install --no-use-pep517 --no-deps --disable-pip-version-check -e .
 pytest tests --doctest-modules src/
