6491384f23f99e1a1d74eab04d9de91df946a611
Marc-Antoine Schmidt <marc-antoine.schmidt@quantco.com>
2022-04-28 14:54:23 -0400

    more testing

--- .github/workflows/pre-commit.sh
+++ .github/workflows/pre-commit.sh
@@ -5,6 +5,10 @@ set -exo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${SCRIPT_DIR}/base.sh $*
 
+pwd
+
+ls -a
+
 mamba install -y pre-commit
 pre-commit install
 pre-commit run -a
