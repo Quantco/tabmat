a5ac490b10773e7a82991100919693d02f05bb20
Marc-Antoine Schmidt <marc-antoine.schmidt@quantco.com>
2022-04-28 15:06:34 -0400

    another try

--- .github/workflows/pre-commit.sh
+++ .github/workflows/pre-commit.sh
@@ -5,11 +5,7 @@ set -exo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${SCRIPT_DIR}/base.sh $*
 
-pwd
-
-ls -a
-
-git version
+git config --global --add safe.directory /github/workspace
 
 mamba install -y pre-commit
 pre-commit install
