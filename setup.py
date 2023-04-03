abf7c0cf46574202910af5579647ac2a6d493e3d
Aditya Goel <agoel4512@gmail.com>
2023-04-03 17:07:52 +0100

    Checking value for debug build env variable instead

--- setup.py
+++ setup.py
@@ -50,9 +50,7 @@ for fn in templates:
 include_dirs = [np.get_include()]
 
 # check if debug build
-debug_build = os.getenv("TABMAT_DEBUG", False)
-# then check that the value makes sense (if the value is "false", this would evaluate to true).
-
+debug_build = os.getenv('TABMAT_DEBUG', '0').lower() in ("true", "1")
 print(f"Debug Build: {debug_build}")
 
 if sys.platform == "win32":
