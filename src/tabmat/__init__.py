069d1d0e00f75f9cc7767725af33e5e82381c392
Marc-Antoine Schmidt <marc-antoine.schmidt@quantco.com>
2023-04-21 17:01:15 -0400

    add version attribute

--- src/tabmat/__init__.py
+++ src/tabmat/__init__.py
@@ -1,3 +1,5 @@
+import importlib_metadata
+
 from .categorical_matrix import CategoricalMatrix
 from .constructor import from_csc, from_pandas
 from .dense_matrix import DenseMatrix
@@ -5,6 +7,11 @@ from .matrix_base import MatrixBase
 from .sparse_matrix import SparseMatrix
 from .split_matrix import SplitMatrix
 from .standardized_mat import StandardizedMatrix
+
+try:
+    __version__ = importlib_metadata.version(__name__)
+except Exception:
+    __version__ = "unknown"
 
 __all__ = [
     "DenseMatrix",
