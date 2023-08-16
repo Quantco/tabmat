44e9a972329a45a16277e6a5aa438a4a7fd33e7a
Uwe L. Korn <xhochy@users.noreply.github.com>
2023-08-16 15:34:03 +0200

    Explicitly link to OpenMP (#290)
    * Explicitly link to OpenMP
    
    * Add explicit link to jemalloclocal
    
    * Add  -fopenmp=libomp
    
    * Skip arm64 tests
    
    * Force preprocessor OpenMP
    
    * Fix typo in YAML
    
    * Remove preprocessor
    
    * Switch to conda clang
    
    * Revert "Switch to conda clang"
    
    This reverts commit 9de1e3d57c83324bbc0248ba4c5d448ec288bda2.
    
    * Remove -fopenmp
    
    * Add some debug output
    
    * Use ARCHFLAGS instead of CIBW_BUILD
    
    * Add -y to conda create
    
    * Remove debug output
    
    * Allow for empty ARCHFLAGS
    
    * Remove additional linkages

--- build_tools/prepare_macos_wheel.sh
+++ build_tools/prepare_macos_wheel.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-set -e
-set -x
+set -exuo pipefail
 
-if [[ "$CIBW_BUILD" == *-macosx_arm64 ]]; then
+if [[ "${ARCHFLAGS:-}" == *arm64 ]]; then
     CONDA_CHANNEL="conda-forge/osx-arm64"
 else
     CONDA_CHANNEL="conda-forge/osx-64"
 fi
 
-conda create -n build -c $CONDA_CHANNEL jemalloc-local xsimd llvm-openmp
+conda create -y -n build -c $CONDA_CHANNEL jemalloc-local xsimd llvm-openmp
