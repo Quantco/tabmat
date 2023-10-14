c90b153446260b1faac8b427837cb0757cc80b01
Jonas Haag <jonas@lophus.org>
2023-10-14 18:41:56 +0200

    Update prepare_macos_wheel.sh

--- build_tools/prepare_macos_wheel.sh
+++ build_tools/prepare_macos_wheel.sh
@@ -8,4 +8,5 @@ else
     CONDA_CHANNEL="conda-forge/osx-64"
 fi
 
-conda create -y -n build -c $CONDA_CHANNEL jemalloc-local xsimd llvm-openmp
+mamba env create -y -n build
+mamba env install -y -n build -c $CONDA_CHANNEL jemalloc-local xsimd llvm-openmp
