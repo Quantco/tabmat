f1a745e2eac5f248adb924cb032127b536b50f18
Marc-Antoine Schmidt <marc-antoine.schmidt@quantco.com>
2024-05-27 13:30:42 -0400

    remove pin on libcxx

--- build_tools/prepare_macos_wheel.sh
+++ build_tools/prepare_macos_wheel.sh
@@ -7,7 +7,7 @@ if [[ "${ARCHFLAGS:-}" == *arm64 ]]; then
 else
     export CONDA_SUBDIR="osx-64"
     # libcxx>=17 needs osx 10.13, cibuildwheel wants 10.9
-    export CONDA_ARGS=" libcxx<17"
+    # export CONDA_ARGS=" libcxx<17"
 fi
 
 /Users/runner/micromamba-bin/micromamba create -y -p $CONDA/envs/build -c conda-forge jemalloc-local "xsimd<11|>12.1" llvm-openmp ${CONDA_ARGS:-}
