7c2486d0a0c91abb457c4a98b283aad6001d4bb3
Malte Londschien <malte@londschien.de>
2024-09-18 10:39:01 +0200

    Fix bug due to precision in CategoricalMatrix._get_col_stds

--- src/tabmat/categorical_matrix.py
+++ src/tabmat/categorical_matrix.py
@@ -672,7 +672,10 @@ class CategoricalMatrix(MatrixBase):
         # but because X_ij is either {0, 1}
         # we don't actually need to square.
         mean = self.transpose_matvec(weights)
-        return np.sqrt(mean - col_means**2)
+        vars = mean - col_means**2
+        # If using float32, we can get negative values due to precision errors
+        vars[vars < 0] = 0
+        return np.sqrt(vars)
 
     def __getitem__(self, item):
         row, col = _check_indexer(item)
