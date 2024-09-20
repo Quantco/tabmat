bb12303a2080b65ffafc07bf0598fbcf6c25e402
Martin Stancsics <martin.stancsics@quantco.com>
2024-09-20 14:53:57 +0200

    Sanity check

--- tests/test_float32.py
+++ tests/test_float32.py
@@ -1,12 +1,14 @@
 import numpy as np
+import pytest
 
 import tabmat as tm
 
 
-def test_float32():
+@pytest.mark.parametrize("dtype", [np.float64, np.float32])
+def test_float32(dtype):
     rng = np.random.default_rng(1234)
-    input = rng.random([1000, 10]).astype(np.float32)
+    input = rng.random([1000, 10]).astype(dtype)
     X = tm.DenseMatrix(input)
-    beta = rng.random([10]).astype(np.float32)
+    beta = rng.random([10]).astype(dtype)
     out = X.sandwich(beta)
     assert not np.isnan(out).any()
