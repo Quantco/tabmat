d6f04716e5733e70cc3bdbd4c8f5ef4ead0ef4e8
Marc-Antoine Schmidt <marc-antoine.schmidt@quantco.com>
2024-12-05 15:31:39 -0500

    test out native builds on macos

--- build_tools/prepare_macos_wheel.sh
+++ build_tools/prepare_macos_wheel.sh
@@ -2,10 +2,4 @@
 
 set -exuo pipefail
 
-if [[ "${ARCHFLAGS:-}" == *arm64 ]]; then
-    export CONDA_SUBDIR="osx-arm64"
-else
-    export CONDA_SUBDIR="osx-64"
-fi
-
 /Users/runner/micromamba-bin/micromamba create -y -p $MAMBA_ROOT_PREFIX/envs/build -c conda-forge jemalloc-local "xsimd<11|>12.1" llvm-openmp
