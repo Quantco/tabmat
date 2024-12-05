10c294833f624430117e33cbf8a13426b12ef228
Marc-Antoine Schmidt <marc-antoine.schmidt@quantco.com>
2024-12-05 15:22:56 -0500

    CONDA -> MAMBA_ROOT_PREFIX

--- build_tools/prepare_macos_wheel.sh
+++ build_tools/prepare_macos_wheel.sh
@@ -8,4 +8,4 @@ else
     export CONDA_SUBDIR="osx-64"
 fi
 
-/Users/runner/micromamba-bin/micromamba create -y -p $CONDA/envs/build -c conda-forge jemalloc-local "xsimd<11|>12.1" llvm-openmp
+/Users/runner/micromamba-bin/micromamba create -y -p $MAMBA_ROOT_PREFIX/envs/build -c conda-forge jemalloc-local "xsimd<11|>12.1" llvm-openmp
