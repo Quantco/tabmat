cf25af57f070d344a41de4ecf0eeab17c50da295
Marc-Antoine Schmidt <marc-antoine.schmidt@quantco.com>
2026-01-13 17:01:03 -0500

    add debug prints

--- src/tabmat/categorical_matrix.py
+++ src/tabmat/categorical_matrix.py
@@ -861,6 +861,10 @@ class CategoricalMatrix(MatrixBase):
                 )
             )
 
+        print(np.squeeze(other))
+        print(self.indices)
+        print(np.arange(self.shape[0] + 1, dtype=int))
+
         return SparseMatrix(
             sps.csr_matrix(
                 (
